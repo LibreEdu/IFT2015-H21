3b5bd98e2c81bdcc5195e49f97f4a50d2a91ada5
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-02 22:16:40 -0500

    typo

--- lindenmayer/src/devoir1/Main.java
+++ lindenmayer/src/devoir1/Main.java
@@ -3,8 +3,8 @@ package devoir1;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Coucou");
-		BidonTest bidonTest = new BidonTest();
+		System.out.println("Coucou :-)");
+		//BidonTest bidonTest = new BidonTest();
 	}
 
 }
