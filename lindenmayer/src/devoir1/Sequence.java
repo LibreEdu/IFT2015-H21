39633c28e132633284aa27f7ff841ec6b50b1b1f
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-12 12:13:03 -0500

    Sequence Test

--- lindenmayer/src/devoir1/Sequence.java
+++ lindenmayer/src/devoir1/Sequence.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 import lindenmayer.Symbol;
 
+/**
+ * Class managing a sequence of symbols
+ * 
+ * @author Alexandre Pachot
+ *
+ */
 public class Sequence implements Symbol.Seq {
 	
 	private List<Symbol> list;
@@ -28,14 +34,23 @@ public class Sequence implements Symbol.Seq {
     	list.add(s);
     }
     
+    /**
+     * Returns the size of the sequence
+     */
     public int size() {
     	return list.size();
     }
     
+    /**
+     * Returns the ith symbol of the sequence
+     */
     public Symbol get(int index) {
     	return list.get(index);
     }
     
+    /**
+     * Replace the ith symbol by a sequence
+     */
     public void set(int index, Symbol.Seq seq) {
     	LSystem s = new LSystem();
     	for(int i = 0; i < seq.size(); i++) {
