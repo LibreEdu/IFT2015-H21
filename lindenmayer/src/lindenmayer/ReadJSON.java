5cb35d88a204bf87a710a94e1e9b1ab15026e92b
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-14 16:59:13 -0500

    ReadJSON

--- lindenmayer/src/lindenmayer/ReadJSON.java
+++ lindenmayer/src/lindenmayer/ReadJSON.java
@@ -14,43 +14,38 @@ public class ReadJSON {
 			throws IOException {
         JSONObject input = new JSONObject(new JSONTokener(
         		new java.io.FileReader(file)));
+        
         JSONArray alphabet = input.getJSONArray("alphabet");
-        JSONObject actions = input.getJSONObject("actions");
-        JSONObject regles = input.getJSONObject("rules");
-        JSONObject parameters = input.getJSONObject("parameters");
-        int step = parameters.getInt("step");
-        double delta = parameters.getDouble("angle");
-        JSONArray start = parameters.getJSONArray("start");
-        double x = start.getDouble(0);
-        double y = start.getDouble(1);
-        double angle = start.getDouble(2);
-        ArrayList<String> listA = new ArrayList<String>(actions.keySet());
-        ArrayList<String> listR = new ArrayList<String>(regles.keySet());
-        t.setUnits(step, delta);
-        t.init(new Point2D.Double(x,y), angle);
-        ls.setAxiom(input.getString("axiom"));
-        // Ajoute l'alphabet
         for (int i = 0; i < alphabet.length(); i++) {
-            String letter = alphabet.getString(i);
-            ls.addSymbol(letter.charAt(0));
-            System.out.println(letter.charAt(0));
+            ls.addSymbol(alphabet.getString(i).charAt(0));
         }
-       
-        for (int i=0; i<listR.size() ; i++) {
-            String key = listR.get(i) ;
-            char clee= key.charAt(0);
-            JSONArray regle = regles.getJSONArray(key);
-            for (int j=0; j<regle.length();j++) {
-            	ls.addRule(clee, regle.getString(j));
-            	System.out.println(clee+" "+regle.getString(j));
+        
+        JSONObject rules = input.getJSONObject("rules");
+        ArrayList<String> symboles = new ArrayList<String>(rules.keySet());
+        String symbole;
+        JSONArray regles;
+        for (int i = 0; i < symboles.size(); i++) {
+        	symbole = symboles.get(i);
+            regles = rules.getJSONArray(symbole);
+            for (int j = 0; j < regles.length(); j++) {
+            	ls.addRule(symbole.charAt(0), regles.getString(j));
             }
         }
-        for(int i=0; i<listA.size();i++) {
-        	String key = listA.get(i);
-        	char clee = key.charAt(0);
-        	ls.setAction(clee, actions.getString(key));
-        	System.out.println(clee +" "+ actions.getString(key));
+        
+        ls.setAxiom(input.getString("axiom"));
+        
+        JSONObject actions = input.getJSONObject("actions");
+        symboles = new ArrayList<String>(actions.keySet());
+        for (int i = 0; i < symboles.size(); i++) {
+        	symbole = symboles.get(i);
+        	ls.setAction(symbole.charAt(0), actions.getString(symbole));
         }
+        
+        JSONObject parameters = input.getJSONObject("parameters");
+        JSONArray start = parameters.getJSONArray("start");
+        Point2D pos = new Point2D.Double(start.getDouble(0),start.getDouble(1));
+        t.init(pos, start.getDouble(2));
+        t.setUnits(parameters.getDouble("step"), parameters.getDouble("angle"));
 	}
 
 }
