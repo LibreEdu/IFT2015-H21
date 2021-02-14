62c67f973b7e99ec1de3ebbe7aa4221f3d544914
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-14 17:17:55 -0500

    Update ReadJSONTest.java

--- lindenmayer/src/lindenmayerTest/ReadJSONTest.java
+++ lindenmayer/src/lindenmayerTest/ReadJSONTest.java
@@ -17,13 +17,31 @@ public class ReadJSONTest {
 	}
 	
 	public void test() {
-        try {
+		System.out.println("  Début test ReadJSON");
+		try {
 			readJSON.readFile("./test/herbe.json", turtle, ls);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
+		System.out.println("    Rule F : " + ls.toString(ls.rewrite(ls.addSymbol('F'))));
+		System.out.println("    Rule F : " + ls.toString(ls.rewrite(ls.addSymbol('F'))));
+		System.out.println("    Rule F : " + ls.toString(ls.rewrite(ls.addSymbol('F'))));
+		System.out.println("    Axiom  : " + ls.toString(ls.getAxiom()));
+		System.out.println("    S0     : " + ls.toString(ls.applyRules(ls.getAxiom(), 0)));
+		System.out.println("    S1     : " + ls.toString(ls.applyRules(ls.getAxiom(), 1)));
+		System.out.println("    S2     : " + ls.toString(ls.applyRules(ls.getAxiom(), 2)));
+		try {
+			readJSON.readFile("./test/sierpinski.json", turtle, ls);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("    Rule L : " + ls.toString(ls.rewrite(ls.addSymbol('L'))));
+		System.out.println("    Rule R : " + ls.toString(ls.rewrite(ls.addSymbol('R'))));
+		System.out.println("    Axiom  : " + ls.toString(ls.getAxiom()));
+		System.out.println("    S0     : " + ls.toString(ls.applyRules(ls.getAxiom(), 0)));
+		System.out.println("    S1     : " + ls.toString(ls.applyRules(ls.getAxiom(), 1)));
+		System.out.println("    S2     : " + ls.toString(ls.applyRules(ls.getAxiom(), 2)));
+		System.out.println("  Fin test ReadJSON");
 	}
 	
 }
