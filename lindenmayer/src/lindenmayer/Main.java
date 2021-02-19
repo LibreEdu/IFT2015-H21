c0e6730a949e5407725e4abc3c46fd8bd3285c5c
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-19 18:01:24 -0500

    clean Main

--- lindenmayer/src/lindenmayer/Main.java
+++ lindenmayer/src/lindenmayer/Main.java
@@ -8,12 +8,10 @@ import javax.swing.SwingUtilities;
 import lindenmayerTest.MainTest;
 
 public class Main {
-	private static LSystem lsystem;
-	private static TortuePS turtle;
-	private static ReadJSON readJSON;
+
 	
 	public static void main(String[] args) {
-		/*System.out.println("coucou");
+		System.out.println("coucou");
 		System.out.println("coucou2");
 		/*
 		MainTest test = new MainTest();
@@ -29,20 +27,6 @@ public class Main {
             }
         });
         //*/
-        lsystem = new LSystem();
-        readJSON = new ReadJSON();
-        turtle = new TortuePS();
-        
-        try {
-			readJSON.readFile(args[0], turtle, lsystem);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        int nbRounds = Integer.parseInt(args[1]);
-        fileStart();
-        Rectangle2D rectangle2D = lsystem.tell(turtle, lsystem.getAxiom(),
-        		nbRounds);
-        fileEnd(Rectangle2D);
 	}
 
 }
