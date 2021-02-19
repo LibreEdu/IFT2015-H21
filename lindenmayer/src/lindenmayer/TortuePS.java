8eab1889bb437861e30958ffb0c250302e0afa0a
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-19 12:53:46 -0500

    TortuePS empty

---
+++ lindenmayer/src/lindenmayer/TortuePS.java
@@ -0,0 +1,13 @@
+package lindenmayer;
+
+public class TortuePS extends AbstractTurtle {
+
+	public TortuePS() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw() {
+
+	}
+}
