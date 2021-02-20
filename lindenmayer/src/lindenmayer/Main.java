e7a7ef954806a662c1ca8e934674861924b96d86
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-20 01:23:53 -0500

    test écran

--- lindenmayer/src/lindenmayer/Main.java
+++ lindenmayer/src/lindenmayer/Main.java
@@ -14,14 +14,14 @@ public class Main {
 		/*
 		MainTest test = new MainTest();
 		test.run();
-		//*/
+		/*/
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                //new MainFrame("./test/buisson.json", 3).setVisible(true);
-                //new MainFrame("./test/herbe.json", 4).setVisible(true);
-                new MainFrame("./test/hexamaze.json", 5).setVisible(true);
-                //new MainFrame("./test/sierpinski.json", 10).setVisible(true);
+               // new MainFrame("./test/buisson.json", 5).setVisible(true);
+                //new MainFrame("./test/herbe.json", 7).setVisible(true);
+                new MainFrame("./test/hexamaze.json", 6).setVisible(true);
+                //new MainFrame("./test/sierpinski.json", 8).setVisible(true);
             }
         });
         //*/
