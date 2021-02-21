139c8b9865b2a2dc23a0afe01b77617f95f62d90
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2021-02-20 22:14:41 -0500

    sh

--- lindenmayer/test2/lindenmayer.sh
+++ lindenmayer/test2/lindenmayer.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 java -jar lindenmayer.jar buisson.json 5 > buisson5.eps
 ps2pdf -dEPSCrop buisson5.eps
-java -jar lindenmayer.jar herbe.json 7 > herbe5.eps
+java -jar lindenmayer.jar herbe.json 7 > herbe7.eps
 ps2pdf herbe7.eps
 java -jar lindenmayer.jar hexamaze.json 6 > hexamaze6.eps
 ps2pdf hexamaze6.eps
