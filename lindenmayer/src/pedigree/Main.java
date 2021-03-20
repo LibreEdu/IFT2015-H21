4c02e328bf54689d0692c48a13118eecc9d2b72c
DS-A8 <78123786+DS-A8@users.noreply.github.com>
2021-03-20 15:58:16 -0400

    Update Main.java

--- lindenmayer/src/pedigree/Main.java
+++ lindenmayer/src/pedigree/Main.java
@@ -7,155 +7,110 @@ import java.util.PriorityQueue;
 import java.util.Random;
 
 import pedigree.Event.Type;
+import pedigree.Sim.Sex;
 
 public class Main {
-	
+
 	private static ArrayList<Sim> population;
+	private static MinHeap min = new MinHeap();
 	private static HashMap<Integer, Integer> vivants;
 	private static PriorityQueue<Event> evenements;
 	private static final int SMP_SIZE = 1000;
+	private static final double  fidelity= 0.9;
+	private static AgeModel ageModel = new AgeModel();
+	private static double span = ageModel.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
+    private static double rate = 2.0/span;
+	private static Random RDM = new Random();
 
 	public static void main(String[] args) {
 		population = new ArrayList<Sim>(SMP_SIZE);
 		vivants = new HashMap<Integer, Integer>(SMP_SIZE);
 		evenements = new PriorityQueue<Event>(SMP_SIZE);
 		
-        
+	}        
 		//System.out.println("test");
         public void simulate(int n, double Tmax){
-            //PQ eventQ = new PQ(); // file de priorité
-        	AgeModel ageModel = new AgeModel();
-    		double[] lifespan = ageModel.lifeSpan(SMP_SIZE);
-    		double span = ageModel.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
-            double rate = 2.0/span;	
             Sim sim;
-            Random random=new Random();
-		    double time;
 		   
-           for (int i=0; i<n; i++)
-           {
-        	   
-        	   if(Math.random() < 0.5) {
-        		   sim = new Sim(Sim.Sex.F);
-   				   Event event = new Event(0.0, sim, Event.Type.Birth);
-   			   	   evenements.add(event);
-   			   	   
-   				}
-   				else {
-   					sim = new Sim(Sim.Sex.M);
-   					Event event = new Event(0.0, sim, Event.Type.Birth);
-        	   		evenements.add(event); // insertion dans la file de priorité
-        	   		
-   				}
-        	   
+           for (int i=0; i<n; i++){
+        	   sim = new Sim(sex());
+        	   Birth(sim,0.0);
            }
-           while (!evenements.isEmpty())
-           {
-              Event E = evenements.poll(); // prochain événement
-              if (E.getYear() > Tmax) break; // arrêter à Tmax
-              if (E.getType() == Event.Type.Death){
-            	  
-            	  if(E.getSim().getSex()== Sim.Sex.M) {
-            		//enlever du tas min si un homme meurt
-            	  }
-            	  continue;
-              } // else rien à faire avec E car son sujet est mort
+           while (!evenements.isEmpty()){
               
-              else if(E.getType() == Event.Type.Birth) {
+        	   Event E = evenements.poll(); // prochain Ã©vÃ©nement
+        	   if (E.getYear() > Tmax) break; // arrÃªter Ã  Tmax
+        	   if (E.getType() == Event.Type.Death){//evenement de mort
+        		   
+        		   if(E.getSim().getSex()== Sim.Sex.M) min.deleteMin(); // homme meurt on l'enleve du tas
+        		   continue;
+              } 
+              else if(E.getType() == Event.Type.Birth) { // evenement de naissance 
             	  
-            	  if(E.getSim().getSex()== Sim.Sex.M) {
-            			  //ajouter au heap
-            	  }
-            	  else {
-            		  
-            		  time = ageModel.randomWaitingTime(random, rate);
-      				  Event event = new Event(time, sim, Event.Type.Mating);
-      				  evenements.add(event);
-            	  }
-            	
-            	  time = ageModel.randomAge(random) + E.getYear();
-            	  sim.setDeath(time);
-            	  population.add(sim.getSim_ident(),sim);
+            	  if(E.getSim().getSex()== Sim.Sex.M) min.insert(E.getSim()); // homme nait on l'ajoute au tas            	  
+            	  else matingTime(E.getSim(),E.getYear());  // femme nait on lui donne un temps reproduction
+            	  death(E.getSim(),E.getYear()); // temps de mort a tous et ajout a la population  
               }
-              else {
-            	  if (E.getYear()>E.getSim().getDeathTime()) continue;
-            	  if (E.getSim().getBirthTime()- Sim.MIN_MATING_AGE_F > 0 &&
-            			  E.getSim().getBirthTime() + E.getYear()< Sim.MAX_MATING_AGE_F) {
-            		  Random RND = new Random(); // générateur de nombres pseudoaléatoires
-            		  Sim x = E.getSim();
+              else { // evenement reproduction
+            	  
+            	  if (E.getYear()>E.getSim().getDeathTime()) continue; // sim mort on ne fait rien
+            	  if (E.getSim().isMatingAge(E.getYear())) { // sim en age de reproduction
+            		  
+            		  Sim x = E.getSim(); //mere
             		  Sim y = null; // choisir pere y
-            		  if (!x.isInARelationship(E.getYear()) || RND.nextDouble()>fidelite)
-            		  { // partenaire au hasard
-            		     do
-            		     {
-            		       Sim z = random sim;
-            		        if (z.isMatingAge(E.getYear())) // isMatingAge() vérifie si z est de l'age acceptable
-            		        {
-            		           if (x.isInARelationship(E.getYear()) // z accepte si x est infidèle
-            		               || !z.isInARelationship(E.getYear())
-            		               || RND.nextDouble()>fidelite)
-            		           {  y = z;}
+            	
+            		  if (!x.isInARelationship(E.getYear()) || RDM.nextDouble() > fidelity){ // partenaire au hasard
+            		     
+            			  do{
+            				  Sim z = random();
+            				  if (z.isMatingAge(E.getYear())) { // isMatingAge() vï¿½rifie si z est de l'age acceptable
+            					  if (x.isInARelationship(E.getYear()) // z accepte si x est infidï¿½le
+            							  || !z.isInARelationship(E.getYear())
+            							  || RDM.nextDouble() > fidelity)
+            		           {  y = z;}	  
             		        }
             		     } while (y==null);
-            		  } else
-            		  {
-            		     y = partenaire précédent de x
+            		  } 
+            		  else{ //partenaire existant
+            		     y = E.getSim().getMate();
             		  }
-            	  }
-            	  time = ageModel.randomWaitingTime(random, rate);
-  				  Event event = new Event(time, sim, Event.Type.Mating);
-  				  evenements.add(event);
-            	  
+            		  //nouveau temps de reproduction
+            		  matingTime(E.getSim(),E.getYear());
+      				  //creation du sim enfant
+      				  sim = new Sim(x,y,E.getYear(),sex());
+      				  Birth(sim,E.getYear());
+            	  }            	  
               }
               
            }
-     
-		for(int i = 0; i < SMP_SIZE; i++) {
-			Sim sim;
-			Random random=new Random();
-			double time;
-			
-			// Choix du sexe
-			if(Math.random() < 0.5) {
-				sim = new Sim(Sim.Sex.F);
-				time = ageModel.randomWaitingTime(random, stable_rate);
-				Event event = new Event(time, sim, Event.Type.Mating);
-				evenements.add(event);
-			}
-			else sim = new Sim(Sim.Sex.M);
 
-			// Ã‚ge de mort
-			sim.setDeath(lifespan[i]);
+        }
+		private void death(Sim sim, double year) {
 			
-			// Ajout Ã  la population de tous les sims depuis la crÃ©ation
-			population.add(sim);
+			double time = ageModel.randomAge(RDM) + year;
+      	    sim.setDeath(time);
+      	    population.add(sim.getSim_ident(),sim);
 			
-			// Ajout Ã  la population des sims vivants
-			vivants.put(sim.getSim_ident(), sim.getSim_ident());
-			
-			// Ajout de lâ€™Ã©vÃ¨nement de mort
-			Event event = new Event(sim.getDeathTime(), sim, Event.Type.Death);
-			evenements.add(event);
 		}
-		
-		/*int i = 0;
-		while(evenements.size()>0) {
-			Event event = evenements.poll();
-			//System.out.println(i++);
-			System.out.println(event.toString());
-		}*/
-		
-		
-		/*for(int i = 0; i < population.size(); i++) {
-			System.out.println(i);
-			System.out.println(population.get(i).toString());
-		}*/
-		
-		/*for(int i = 0; i < vivants.size(); i++) {
-			System.out.println(i);
-			System.out.println(vivants.get(i).toString());
-		}*/
-	}
+		private void matingTime(Sim sim, double year) {
+			double time = ageModel.randomWaitingTime(RDM, rate)+ year;
+			Event event = new Event(time, sim, Event.Type.Mating);
+			evenements.add(event);
+			
+		}
+		private void Birth(Sim sim, double t) {
+			Event event = new Event(t, sim, Event.Type.Birth);
+		   	evenements.add(event);
+			
+		}
+		private Sex sex() {
+			if (RDM.nextDouble()>0.5) return Sim.Sex.F;
+			else return Sim.Sex.M;
+		}
+		private Sim random() {
+			int i = RDM.nextInt(min.size());
+			return min.getSim(i);
+		}
 
 }
-}
+
