17b44492b3f9fdabeac222f36f2a696c3f8a0416
Alexandre Pachot <pachot@gmail.com>
2021-03-23 14:51:51 -0400

    del birtComparator

--- lindenmayer/src/pedigree/birthComparator.java
+++
@@ -1,9 +0,0 @@
-package pedigree;
-
-import java.util.Comparator;
-
-public class BirthComparator<T> implements Comparator<Sim> {
-    public int compare(Sim s1, Sim s2) {
-        return s2.compareBirth(s1);
-    }
-}
