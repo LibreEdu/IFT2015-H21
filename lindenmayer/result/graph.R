9870d499b2f0ca80b69fd86c95c6088019372fec
Alexandre Pachot <pachot@gmail.com>
2021-03-24 12:57:31 -0400

    bug data vide / plot

--- lindenmayer/result/graph.R
+++ lindenmayer/result/graph.R
@@ -19,7 +19,7 @@ par(new=TRUE)
 plot(x=x3, y=y3, xlim=c(0,xmax), ylim=c(1,ymax), log="y", pch=22, col=color3, bg=color3, ylab = "", xlab = "")
 
 lines(x1[order(x1)], y1[order(x1)], xlim=range(x1), ylim=range(y1), lwd=1, col=color1)
-lines(x2[order(x2)], y2[order(x2)], xlim=range(x2), ylim=range(y2), lwd=1, col=color2)
-lines(x3[order(x3)], y3[order(x3)], xlim=range(x3), ylim=range(y3), lwd=1, col=color3)
+if (is.data.frame(get('y2'))) lines(x2[order(x2)], y2[order(x2)], xlim=range(x2), ylim=range(y2), lwd=1, col=color2)
+if (is.data.frame(get('y3'))) lines(x2[order(x2)], y2[order(x2)], xlim=range(x2), ylim=range(y2), lwd=1, col=color2)
 
 legend("left", legend=c("Population size", "Forefathers", "Foremothers"), col=c(color1, color2, color3), lty=1:2, cex=0.5)
