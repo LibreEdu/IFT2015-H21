33423bbe988fec954efd492170ffc539c51382f6
Alexandre Pachot <pachot@gmail.com>
2021-03-24 10:58:56 -0400

    Script avec variables

--- lindenmayer/result/data.R
+++ lindenmayer/result/data.R
@@ -1,9 +1,10 @@
 pdf("plot.pdf")
 par(mfrow=c(2,2))
+directory<-"./data"
 for (i in seq(1,4)) {
-  data1 <- read.delim(paste("./data/data", i, "a.txt", sep=""))
-  data2 <- read.delim(paste("./data/data", i, "b.txt", sep=""))
-  data3 <- read.delim(paste("./data/data", i, "c.txt", sep=""))
+  data1 <- read.delim(paste(directory, "/data", i, "a.txt", sep=""))
+  data2 <- read.delim(paste(directory, "/data", i, "b.txt", sep=""))
+  data3 <- read.delim(paste(directory, "/data", i, "c.txt", sep=""))
   source("./graph.R")
 }
 dev.off()
